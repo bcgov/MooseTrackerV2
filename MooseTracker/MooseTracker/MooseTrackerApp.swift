c31864fc4eac631dcd0a3695729df18ee5d4646a
Luna Morris <5862lunamoon@gmail.com>
2024-02-01 10:37:23 -0800

    inital commit

---
+++ MooseTracker/MooseTracker/MooseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooseTrackerApp.swift
+//  MooseTracker
+//
+//  Created by Luna Morris on 2024-02-01.
+//
+
+import SwiftUI
+
+@main
+struct MooseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
