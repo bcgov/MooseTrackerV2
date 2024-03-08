43d92d3474d5c218a579fa7d6d4faaa5a75e4464
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2024-03-07 16:48:56 -0800

    Add red moose for unsynced moose and edit logic

--- app/src/UI/Map.tsx
+++ app/src/UI/Map.tsx
@@ -32,20 +32,42 @@ const mooseIconCalf = new Icon({
 });
 
 const mooseNotSyncIconMale = new Icon({
-  iconUrl: "moosegreen.png",
+  iconUrl: "moosered.png",
   iconSize: [30, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
 const mooseNotSyncIconFemale = new Icon({
-  iconUrl: "fmoosegreen.png",
+  iconUrl: "fmoosered.png",
   iconSize: [30, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
 const mooseNotSyncIconCalf = new Icon({
+  iconUrl: "calfred.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+const mooseSyncedIconMale = new Icon({
+  iconUrl: "moosegreen.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const mooseSyncedIconFemale = new Icon({
+  iconUrl: "fmoosegreen.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const mooseSyncedIconCalf = new Icon({
   iconUrl: "calfgreen.png",
   iconSize: [30, 41],
   iconAnchor: [12, 41],
@@ -112,26 +134,39 @@ const MapMarkers = (props: any) => {
   };
 
   const getMooseIcon = (moose: any, status: any) => {
-    if (status && status === 1) {
+    if (status && status === "Current") {
+      if (moose.age === "Calf" && moose.age !== null) {
+        return mooseIconCalf;
+      }
+      if (moose.gender === "female") {
+        return mooseIconFemale;
+      } else {
+        return mooseIconMale;
+      }
+    } else if (status && status === "Synced") {
+      if (moose.age === "Calf" && moose.age !== null) {
+        return mooseSyncedIconCalf;
+      }
+      if (moose.gender === "male") {
+        return mooseSyncedIconMale;
+      }
+      if (moose.gender === "female") {
+        return mooseSyncedIconFemale;
+      } else {
+        return mooseSyncedIconMale;
+      }
+    } else {
       if (moose.age === "Calf" && moose.age !== null) {
         return mooseNotSyncIconCalf;
+      }
+      if (moose.gender === "male") {
+        return mooseNotSyncIconMale;
       }
       if (moose.gender === "female") {
         return mooseNotSyncIconFemale;
       } else {
         return mooseNotSyncIconMale;
       }
-    }
-    if (moose.age === "Calf" && moose.age !== null) {
-      return mooseIconCalf;
-    }
-    if (moose.gender === "male") {
-      return mooseIconMale;
-    }
-    if (moose.gender === "female") {
-      return mooseIconFemale;
-    } else {
-      return mooseIconMale;
     }
   };
 
@@ -140,7 +175,7 @@ const MapMarkers = (props: any) => {
     <>
       {mooseArray.map((moose: any, index: number) => {
         const position = getOffsetLocation(index, markerPosition);
-        const mooseIcon = getMooseIcon(moose, 0);
+        const mooseIcon = getMooseIcon(moose, "Current");
         return (
           <Marker key={index} position={position} icon={mooseIcon} />
         );
@@ -167,7 +202,7 @@ const MapMarkers = (props: any) => {
         return sighting.mooseArray.map((moose: any, mooseIndex: number) => {
           const offLoc: [number, number] = sighting?.location?.latitude ? [sighting.location.latitude, sighting.location.longitude] : markerPosition;
           const position = getOffsetLocation(index, offLoc);
-          const mooseIcon = getMooseIcon(moose, 1);
+          const mooseIcon = getMooseIcon(moose, sighting?.status);
           const mooseDate = new Date(sighting.dateOfSighting).toLocaleDateString();
           return (
             <Marker key={index + mooseIndex} position={position} icon={mooseIcon} >
