9f6adacfce38f6f39ddc27daca9658ce95f994d5
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-08-22 11:39:44 -0700

    leaflet func for center

--- app/src/UI/Map.tsx
+++ app/src/UI/Map.tsx
@@ -1,11 +1,11 @@
-import "./Map.css"
-import { MapContainer, TileLayer, useMap, GeoJSON} from 'react-leaflet'
-import { LatLngExpression} from 'leaflet'
+import "./Map.css";
+import { MapContainer, TileLayer, useMap, GeoJSON } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
 import { useSelector } from "react-redux";
 import L from "leaflet";
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import mgmtUnits from '../assets/management_units.json'
+import mgmtUnits from "../assets/management_units.json";
 import { managementUnitStyle } from "./featureStylers";
 import { FeatureCollection, Feature } from "geojson";
 
@@ -33,33 +33,18 @@ const getFeatureCentroidFromManagementArea = (regionId: string) => {
   const feature = features.find(
     (feature: any) => feature.properties.WILDLIFE_MGMT_UNIT_ID === regionId
   );
-  return feature ? calculateCentroid(feature.geometry.coordinates[0]) : null;
-};
 
-const calculateCentroid = (coordinates: any) => {
-  let xSum = 0;
-  let ySum = 0;
-  let area = 0;
-
-  for (let i = 0; i < coordinates.length - 1; i++) {
-    const [x1, y1] = coordinates[i];
-    const [x2, y2] = coordinates[i + 1];
-
-    const crossProduct = x1 * y2 - x2 * y1;
-    xSum += (x1 + x2) * crossProduct;
-    ySum += (y1 + y2) * crossProduct;
-    area += crossProduct;
+  if (feature) {
+    const layer = L.geoJSON(feature);
+    const bounds = layer.getBounds();
+    const center = bounds.getCenter();
+    return [center.lat, center.lng];
   }
 
-  area *= 0.5;
-  const centroidX = xSum / (6 * area);
-  const centroidY = ySum / (6 * area);
-  return [centroidY, centroidX];
+  return null;
 };
 
-
 export const MapPanel: React.FC = () => {
-
   const defaultLocation: [number, number] = [53.932, -123.912];
 
   const subRegion = useSelector(
@@ -83,7 +68,7 @@ export const MapPanel: React.FC = () => {
         className: `mgmtUnitLabel-${feature.properties.WILDLIFE_MGMT_UNIT_ID}`,
       });
     }
-  }
+  };
   return (
     <div className="MapPanel">
       <MapContainer
@@ -97,7 +82,11 @@ export const MapPanel: React.FC = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={mgmtUnits as FeatureCollection} style={managementUnitStyle} onEachFeature={onEachFeature}/>
+        <GeoJSON
+          data={mgmtUnits as FeatureCollection}
+          style={managementUnitStyle}
+          onEachFeature={onEachFeature}
+        />
         <ChangeView center={markerPosition} />
       </MapContainer>
       <Outlet />
