298ddc06509ed6490fc3d4bb65fdf7f77c46f049
Micheal Wells <micheal.w.wells@gmail.com>
2024-09-06 14:25:20 -0700

    Finish replacing form useState vars with redux vars, some css grid work WIP

--- app/src/UI/Form.tsx
+++ app/src/UI/Form.tsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   ACTIVITY_UPDATE_SIGHTING,
   CLEAR_CURRENT_MOOSE_SIGHTING,
-  USER_SAVE_SIGHTINGS
+  USER_SAVE_SIGHTINGS,
 } from "../state/actions";
 import { RegionSelector } from "./RegionSelector";
 
@@ -12,38 +12,51 @@ export const FormPanel = () => {
   const ref = useRef(0);
   ref.current += 1;
 
-  const bullCount = useSelector((state: any) => state.MooseSightingsState.bullCount)
-  const [cowCount, setCowCount] = useState(0);
-  const [calfCount, setCalfCount] = useState(0);
-  const [unknownCount, setUnknownCount] = useState(0);
-  const [hoursOut, sethoursOut] = useState(0);
-  
+  const bullCount = useSelector(
+    (state: any) => state.MooseSightingsState.bullCount
+  );
+  const cowCount = useSelector(
+    (state: any) => state.MooseSightingsState.cowCount
+  );
+  const calfCount = useSelector(
+    (state: any) => state.MooseSightingsState.calfCount
+  );
+  const unknownCount = useSelector(
+    (state: any) => state.MooseSightingsState.unknownCount
+  );
+  const hoursOut = useSelector(
+    (state: any) => state.MooseSightingsState.hoursOut
+  );
+
   /**
    * Increments/decrements the observation count for a given counter.
    * @param useStateVar the state variable to increment
    * @param useStateSetter the setter associated with the state variable
    * @param incrementBool true if incrementing, false if decrementing
    */
-  const updateCount = (stateVar: any, incrementBool: boolean, payloadName: string) => {
-    let payload: any = {}
+  const updateCount = (
+    stateVar: any,
+    incrementBool: boolean,
+    payloadName: string
+  ) => {
+    let payload: any = {};
     if (incrementBool) {
-      payload[payloadName] = stateVar + 1
+      payload[payloadName] = stateVar + 1;
+    } else {
+      if (stateVar > 0) payload[payloadName] = stateVar - 1;
     }
-    else {
-      if (stateVar > 0)
-        payload[payloadName] = stateVar - 1
-    }
-      dispatch({type: ACTIVITY_UPDATE_SIGHTING, payload: payload})
-  }
+    dispatch({ type: ACTIVITY_UPDATE_SIGHTING, payload: payload });
+  };
 
   const dispatch = useDispatch();
 
-  const date = useSelector(
-    (state: any) => state.MooseSightingsState.date
-  );
+  const date = useSelector((state: any) => state.MooseSightingsState.date);
 
   const handlefromDateChange = (event: any) => {
-    dispatch({ type: ACTIVITY_UPDATE_SIGHTING, payload: { date: event.target.valueAsDate }});
+    dispatch({
+      type: ACTIVITY_UPDATE_SIGHTING,
+      payload: { date: event.target.valueAsDate },
+    });
   };
 
   const handleSubmit = () => {
@@ -52,78 +65,137 @@ export const FormPanel = () => {
 
   return (
     <div className="formPanel">
-      <div className="formContainer" >
+      <div className="formContainer">
         <div className="formHeader">
           <h2>Record Moose Sighting</h2>
           <p>Please enter your moose sightings for your day</p>
         </div>
         <div className="formInput">
-          <div>
-            <label>
-              <span className="formContent">Bulls: </span>
-              <span>{bullCount} </span>
-            </label>
+          <label>
+            <span className="formContent">Bulls: </span>
+          </label>
+          <div>{bullCount} </div>
+        <div className="columnRight">
+            <button
+              className="countBtn"
+              onClick={() => updateCount(bullCount, true, "bullCount")}
+            >
+              +
+            </button>
+            <button
+              className="countBtn"
+              onClick={() => updateCount(bullCount, false, "bullCount")}
+            >
+              -
+            </button>
           </div>
-          <div>
-              <button className="countBtn" onClick={() => updateCount(bullCount, false, "bullCount")}>-</button>
-              <button className="countBtn" onClick={() => updateCount(bullCount, true, "bullCount")}>+</button>
+
+          <label>
+            <span className="formContent">Cows: </span>
+          </label>
+          <div>{cowCount} </div>
+        <div className="columnRight">
+            <button
+              className="countBtn"
+              onClick={() => updateCount(cowCount, true, "cowCount")}
+            >
+              +
+            </button>
+            <button
+              className="countBtn"
+              onClick={() => updateCount(cowCount, false, "cowCount")}
+            >
+              -
+            </button>
           </div>
-            {/*}
-          <div>
-            <label>
-              <span className="formContent">Cows: </span>
-              <span>{cowCount} </span>
-              <button className="countBtn" onClick={() => updateCount(cowCount, setCowCount, false, "cowCount")}>-</button>
-              <button className="countBtn" onClick={() => updateCount(cowCount, setCowCount, true, "cowCount")}>+</button>
-            </label>
+
+          <label>
+            <span className="formContent">Calf: </span>
+          </label>
+          <div>{calfCount} </div>
+        <div className="columnRight">
+            <button
+              className="countBtn"
+              onClick={() => updateCount(calfCount, true, "calfCount")}
+            >
+              +
+            </button>
+            <button
+              className="countBtn"
+              onClick={() => updateCount(calfCount, false, "calfCount")}
+            >
+              -
+            </button>
           </div>
-          <div>
-            <label>
-              <span className="formContent">Calf: </span>
-              <span>{calfCount} </span>
-              <button className="countBtn" onClick={() => updateCount(calfCount, setCalfCount, false, "calfCount")}>-</button>
-              <button className="countBtn" onClick={() => updateCount(calfCount, setCalfCount, true, "calfCount")}>+</button>
-            </label>
+
+          <label>
+            <span className="formContent">Unidentified: </span>
+          </label>
+          <div>{unknownCount} </div>
+        <div className="columnRight">
+            <button
+              className="countBtn"
+              onClick={() => updateCount(unknownCount, true, "unknownCount")}
+            >
+              +
+            </button>
+            <button
+              className="countBtn"
+              onClick={() => updateCount(unknownCount, false, "unknownCount")}
+            >
+              -
+            </button>
           </div>
-          <div>
-            <label>
-              <span className="formContent">Unidentified: </span>
-              <span>{unknownCount} </span>
-              <button className="countBtn" onClick={() => updateCount(unknownCount, setUnknownCount, false, "unknownCount")}>-</button>
-              <button className="countBtn" onClick={() => updateCount(unknownCount, setUnknownCount, true, "unknownCount")}>+</button>
-            </label>
-          </div>
-          <div>
-            <label>
-            <span className="formContent"/>
-              <RegionSelector/>
-            </label>
-          </div>
-          <div>
-            <label>
-              <span className="formContent">Date: </span>
-              <input type="date" id="date" value={date ? date.toISOString().split('T')[0] : ''} onChange={handlefromDateChange}/>
-            </label>
-          </div>          
-          <div>
-            <label>
-              <span className="formContent">Hours Out: </span>
-              <span>{hoursOut} </span>
-              <button className="countBtn" onClick={() => updateCount(hoursOut, sethoursOut, false, "hoursOut")}>-</button>
-              <button className="countBtn" onClick={() => updateCount(hoursOut, sethoursOut, true, "hoursOut")}>+</button>
-            </label>
-          </div>
-        */}
-        </div>
-        <div className="formButtons">
-          <button className="formButton" onClick={() => { dispatch({ type: CLEAR_CURRENT_MOOSE_SIGHTING })}}>
-            Reset
+
+
+          <label>
+            <span className="formContent">Date: </span>
+          </label>
+          <input
+            type="date"
+            id="date"
+            value={date ? date.toISOString().split("T")[0] : ""}
+            onChange={handlefromDateChange}
+          />
+          <div></div>
+
+        <label>
+          <span className="formContent">Hours Out: </span>
+        </label>
+        <div>{hoursOut} </div>
+        <div className="columnRight">
+          <button
+            className="countBtn"
+            onClick={() => updateCount(hoursOut, true, "hoursOut")}
+          >
+            +
           </button>
-          <button className="formButton" onClick={handleSubmit} >
-            Save sighting
+          <button
+            className="countBtn"
+            onClick={() => updateCount(hoursOut, false, "hoursOut")}
+          >
+            -
           </button>
         </div>
+
+        </div>
+            <label>
+              <span className="formContent" />
+            </label>
+              <RegionSelector />
+
       </div>
+      <button
+        className="formButton"
+        onClick={() => {
+          dispatch({ type: CLEAR_CURRENT_MOOSE_SIGHTING });
+        }}
+      >
+        Reset
+      </button>
+      <button className="formButton" onClick={handleSubmit}>
+        Save sighting
+      </button>
     </div>
   );
 };
