fd1dcad8be67cd844fc2d1c993b26758eb02525e
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2024-09-12 14:46:40 -0700

    Remove manual plugin registration
    No longer necessary - plugins are auto-loaded as of Capacitor v3

--- app/android/app/src/main/java/com/example/app/MainActivity.java
+++ app/android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,15 +1,6 @@
 package com.example.app;
 
 import com.getcapacitor.BridgeActivity;
-import com.example.capacitorzoomandroid.ZoomPlugin;
-import com.getcapacitor.Plugin;
-import android.os.Bundle;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-    super.onCreate(savedInstanceState);
-        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-            add(ZoomPlugin.class);
-        }});
-    }
 }
