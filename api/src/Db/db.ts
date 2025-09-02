07f1148027c13e4111e5557f0548dc55f3c7445a
acatchpole <adamantiumite@gmail.com>
2025-09-02 15:57:35 -0700

    add tls vars to db connection pool

--- api/src/Db/db.ts
+++ api/src/Db/db.ts
@@ -9,6 +9,13 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.TLS_CA,
+    key: process.env.TLS_KEY,
+    cert: process.env.TLS_CERT
+  },
+
 });
 
 export async function openDb() {
